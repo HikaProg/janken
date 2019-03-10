7ea2ac8a85ed1fd54aeedbced6fcc67ad8ef2a44
HikaProg <37097257+HikaProg@users.noreply.github.com>
2019-03-10 21:39:12 +0900

    判定修正 ランダム関数作り直し

--- janken/janken/jenken.cpp
+++ janken/janken/jenken.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <time.h>
 #include <map>
+#include <stdio.h>
 using namespace std;
 
 	enum JANKENTYPE
@@ -10,13 +11,24 @@ using namespace std;
 		GUU,
 		TYOKI,
 		PAA,
-		JANKEN_TYPEMAX
+		JANKEN_TYPEMAX,
 	};
 
-struct Janken
+	enum RESULTTYPE
+	{
+		WIN,
+		LOSE,
+		DRAW,
+		RESULT_TYPEMAX,
+	};
+
+#define RESULT_INFOMAX 30
+
+struct JankenKind
 {
-	Janken();
-	Janken(JANKENTYPE myHand, JANKENTYPE winHand, JANKENTYPE loseHand, string myHandStr)
+public:
+	JankenKind();
+	JankenKind(JANKENTYPE myHand, JANKENTYPE winHand, JANKENTYPE loseHand, string myHandStr)
 	{
 		m_myHandStr = myHandStr;
 		judgeInfo[(int)myHand] = "引き分け";
@@ -28,75 +40,140 @@ public:
 	string m_myHandStr;
 };
 
+// じゃんけんの出した手
+class JankenHand
+{
+public:
+	JankenHand()
+	{
+		m_myHand = NULL;
+	}
+	~JankenHand();
+
+	// 出した手を設定
+	void SetMyHand(JankenKind *myHand)
+	{
+		m_myHand = myHand;
+	}
+	// 出した手を取得
+	JankenKind *GetMyHand()
+	{
+		return m_myHand;
+	}
+
+private:
+	JankenKind *m_myHand;
+
+};
+
 struct Rand
 {
 	bool flag;
 	int GetRandom(int min, int max)
 	{
-		if (!flag)
-		{
-			srand((unsigned int)time(NULL));
-			flag = true;
-		}
-		return min + (int)rand()*(max - min + 1.0) / (1.0 + RAND_MAX);
+		std::random_device rnd;
+		std::mt19937_64 mt(rnd());
+		std::uniform_int_distribution<> randRange(min, max);
+		return randRange(mt);
 	}
 };
 
-struct Result
+
+class Result
 {
-	string result[30];
+public:
+	Result() {
+		m_resultType["勝ち"] = WIN;
+		m_resultType["負け"] = LOSE;
+		m_resultType["引き分け"] = DRAW;
+	}
+	~Result();
+	void Init(int num)
+	{
+		m_minNum = num;
+	}
 	// リザルトの追加
 	void AddResult(int num, string _result)
 	{
-		result[num] = _result;
+		m_results[num] = _result;
+		int resultType = m_resultType[_result];
+		m_resultsDetail[resultType]++;
+		m_maxNum = m_results.size() + m_minNum;
 	}
 	// リザルトの表示
-	void ShowResult()
+	void ShowResultsDetail()
 	{
-		int resultMax = sizeof(result) / sizeof(string);
-		for (int i = 1; i <= resultMax; i++)
+		for (int i = m_minNum; i < m_maxNum; i++)
 		{
-			cout << i << "戦目" << result[i].c_str() << "\n";
+			cout << i + 1 << "戦目" << m_results[i].c_str() << "\n";
 		}
 	}
+	void ShowResults()
+	{
+		int w = m_resultsDetail[WIN];
+		int l = m_resultsDetail[LOSE];
+		int d = m_resultsDetail[DRAW];
+		cout << "勝ち:" << w << "," << "負け:" << l << "," << "引き分け" << d << "\n";
+	}
+private:
+	map<int, string> m_results;
+	int m_resultsDetail[RESULT_TYPEMAX];
+	map<string, int> m_resultType;
+	int m_minNum;
+	int m_maxNum;
 };
 
 int main()
 {
-	Janken *jankenHand[JANKEN_TYPEMAX];
-	jankenHand[GUU] = new Janken(GUU, TYOKI, PAA, "グー");
-	jankenHand[TYOKI] = new Janken(TYOKI, PAA, GUU, "チョキ");
-	jankenHand[PAA] = new Janken(PAA, GUU, TYOKI, "パー");
+	JankenKind *jankenHand[JANKEN_TYPEMAX];
+	jankenHand[GUU] = new JankenKind(GUU, TYOKI, PAA, "グー");
+	jankenHand[TYOKI] = new JankenKind(TYOKI, PAA, GUU, "チョキ");
+	jankenHand[PAA] = new JankenKind(PAA, GUU, TYOKI, "パー");
+
+	// 戦績
+	Result *result = new Result();
+	JankenHand *user = new JankenHand();
+	JankenHand *pc = new JankenHand();
+	Rand rnd;
+	int pcHandNum;
+	int myHandNum;
 
 	char userHand[256];
-	// 戦績
-	Result result;
 	int num = 0;
+	// リザルトの初期化
+	result->Init(num);
 	while (true)
 	{
-		cout << "1:グー, 2:チョキ, 3:パーのいずれかを入力してください\nRは戦績を表示します\n";
+		cout << "0未選択, 1:グー, 2:チョキ, 3:パーのいずれかを入力してください\nRは戦績を表示します\n";
 		cin >> userHand;
 		// ユーザーの入力
-		int myHandNum = atoi(userHand);
-		
+		myHandNum = atoi(userHand);
 		if (myHandNum <= 0 || myHandNum > JANKEN_TYPEMAX)
 		{
-			cout << "1,2,3以外は戦績につきません。\n";
-			if(strcmp(userHand, "R") == 0)
-				result.ShowResult();
+			cout << "1:グー, 2:チョキ, 3:パー以外は戦績につきません。\n";
+			if (strcmp(userHand, "R") == 0)
+			{
+				result->ShowResultsDetail();
+				result->Init(num);
+			}
 			continue;
 		}
 		myHandNum = myHandNum - 1;
-		Rand rnd;
 		// PCの結果
-		int pcHandNum = rnd.GetRandom(GUU, PAA);
-		cout << "PCは" << jankenHand[pcHandNum]->m_myHandStr.c_str() << "です。\n";
-		string judge;
+		pcHandNum = rnd.GetRandom(GUU, PAA);
+		// じゃんけんの手を設定
+		user->SetMyHand(jankenHand[myHandNum]);
+		pc->SetMyHand(jankenHand[pcHandNum]);
+		cout << "あなたは" << user->GetMyHand()->m_myHandStr.c_str() << "です。\n";
+		cout << "PCは" << pc->GetMyHand()->m_myHandStr.c_str() << "です。\n";
 		// 勝敗判定
-		judge = jankenHand[myHandNum]->judgeInfo[pcHandNum];
+		string judge = user->GetMyHand()->judgeInfo[pcHandNum];
 		cout << "結果は" << judge.c_str() << "\n";
-		num++;
+
 		// リザルト保存
-		result.AddResult(num, judge);
+		result->AddResult(num, judge);
+		// 戦績表示
+		result->ShowResults();
+		num++;
 	}
 }
